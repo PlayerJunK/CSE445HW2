60857f06ce13037fe8f9622e223258fddcca6ccd
Ryan Prinsen <ryanprinsen3@gmail.com>
2014-09-28 15:50:28 -0700

    Update for MultiCellBuffer

--- CSE445HW2/CSE445HW2/MultiCellBuffer.cs
+++ CSE445HW2/CSE445HW2/MultiCellBuffer.cs
@@ -9,25 +9,33 @@ namespace CSE445HW2
 {
     class MultiCellBuffer
     {
+        struct cell{
+            public int id;
+            public Object Order = new Object();
+        }
         //Establish a reference for the lock. Scope of lock will be limited to the methods inside this class.
         private System.Object myLock = new System.Object();
-        
-        private Order [] orderBuffer = new Order [3];
-        static int semaphore = 0;
+        private cell[] orderBuffer = new cell[3];
+        int bufferCapacity;
+        const int BUFFERSIZE = 3;
+     
         public MultiCellBuffer()
         {
-            orderBuffer[0] = null;
-            orderBuffer[1] = null;
-            orderBuffer[2] = null;
+            bufferCapacity = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                orderBuffer[i].id = -1;
+                orderBuffer[i].Order = null;
+            }   
         }
 
         /*Lock the method so only one thread will be able to access. If there are already 3 orders in the buffer,
         the thread will wait until an order has been removed and is notified (PulseAll).*/
-        public void setOneCell(Order o)
+        public void addObjectwithID(int id, Object o)
         {
             lock (myLock)
             {
-                while (semaphore >= 3)
+                while (bufferCapacity >= BUFFERSIZE)
                 {
                     try
                     {
@@ -35,19 +43,27 @@ namespace CSE445HW2
                     }
                     catch { }
                 }
-                orderBuffer[semaphore] = o;
-                semaphore++;
+                for (int i = 0; i <= BUFFERSIZE; i++)
+                {
+                    if (orderBuffer[i].Order == null)
+                    {
+                        orderBuffer[i].id = id;
+                        orderBuffer[i].Order = o;
+                    }
+                }
+                bufferCapacity++;  
                 Monitor.PulseAll(this);
             }
         }
 
         /*Lock the method so only one thread will be able to access. If there are no orders in the buffer,
         the thread will wait until an order has been entered and is notified (PulseAll).*/
-        public Order getOneCell()
+        public Object getObjectwithID(int id)
         {
             lock (myLock)
             {
-                while (semaphore <= 0)
+                cell temp = new cell();
+                while (bufferCapacity <= 0)
                 {
                     try
                     {
@@ -55,12 +71,24 @@ namespace CSE445HW2
                     }
                     catch { }
                 }
-                semaphore--;
+                for (int i = 0; i <= BUFFERSIZE; i++)
+                {
+                    if (orderBuffer[i].id == id)
+                    {
+                        temp = orderBuffer[i];
+                        bufferCapacity--;
+                        orderBuffer[i].id = -1;
+                        orderBuffer[i].Order = null;
+                        Monitor.PulseAll(this);
+                        return temp.Order;
+                    }
+                }
+               
                 Monitor.PulseAll(this);
-                return this.orderBuffer[semaphore];
+                return null;
             }
         }
-        
+
 
     }
 }
