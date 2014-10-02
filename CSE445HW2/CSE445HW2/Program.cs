93506de7a0926fc085b58ee1e73323845a7cf2d0
John Robertson <jj.robertson@asu.edu>
2014-10-02 13:45:38 -0700

    User input now determines number of Suppliers and Number of Agencies.

--- CSE445HW2/CSE445HW2/Program.cs
+++ CSE445HW2/CSE445HW2/Program.cs
@@ -12,8 +12,46 @@ namespace CSE445HW2
     {
         static void Main(string[] args)
         {
-            int NUM_SUPPLIERS = 3;
-            int NUM_AGENCIES = 5;
+            
+            //instantiate these to negative numbers to know if they are valid or not
+            int NUM_SUPPLIERS = -1;
+            int NUM_AGENCIES = -1;
+
+            //num agenecies and num suppliers must be positive integers
+
+            //read number of suppliers from keyboard
+            while (NUM_SUPPLIERS < 1)
+            {
+                //prompt the user
+                Console.Write("Number of Hotel Suppliers: ");
+
+                //assign input from console to variable
+                NUM_SUPPLIERS = readIntFromConsole();
+
+                //raise error string if input integer is less than 0
+                if (NUM_SUPPLIERS < 1)
+                {
+                    Console.WriteLine("Input must be greater than 0");
+                }
+            }
+
+
+            //read number of agencies from keyboard
+            //loop until an interger larger than 0 is inputted
+            while (NUM_AGENCIES < 1)
+            {
+                //prompt user
+                Console.Write("Number of Agencies: ");
+
+                //read int
+                NUM_AGENCIES = readIntFromConsole();
+
+                //raise error for invalid input
+                if (NUM_AGENCIES < 1)
+                {
+                    Console.WriteLine("Input must be greater than 0");
+                }
+            }
 
             //set up TravelAgency Objects
             ArrayList travelAgencyObjects = new ArrayList();
@@ -59,6 +97,38 @@ namespace CSE445HW2
 
             Console.ReadLine();
 
+        }
+
+
+
+        //Helper method to read an integer from console
+        private static int readIntFromConsole(){
+            int result = -1; ;
+
+            //boolean to track valid input
+            Boolean inputReceived = false;
+
+            //loop until input is valid
+            while (!inputReceived)
+            {
+                //read line of input
+                string input = Console.ReadLine();
+
+                //try to parse input as integer
+                try
+                {
+                    result = Int32.Parse(input);
+                    //if parse is successful break out of loop.
+                    inputReceived = true;
+                }
+                catch (Exception e)  //catch error where input is a non integer
+                {
+                    //print error message
+                    Console.WriteLine("Please enter an integer.");
+                }
+            }
+
+            return result;
         }
     }
 }
